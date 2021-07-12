1bd8386b45c5eaf9517d7ba5d351f52740d8ca47
itsgeraldwang <captainxep@gmail.com>
2021-07-12 14:21:14 -0500

    Added site

---
+++ precache-manifest.4b4e9e18f7fd3f26d8095730f656d5d6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d600a372ce7fb513bd9c007598ec7666",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40b44c4450241c978173",
+    "url": "/static/css/main.a80ee232.chunk.css"
+  },
+  {
+    "revision": "fd4f446118032cc9f740",
+    "url": "/static/js/2.acc3c080.chunk.js"
+  },
+  {
+    "revision": "fde5e0e09ffb7340f7c2a67864a63f47",
+    "url": "/static/js/2.acc3c080.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40b44c4450241c978173",
+    "url": "/static/js/main.41950d49.chunk.js"
+  },
+  {
+    "revision": "422c566574b757aea947",
+    "url": "/static/js/runtime-main.4faf09b8.js"
+  },
+  {
+    "revision": "8c56f2d6ad71d0b803f6eb50184ebf69",
+    "url": "/static/media/a440.8c56f2d6.jpg"
+  },
+  {
+    "revision": "d797f90ce785a9c3ba76f22683e9a0d5",
+    "url": "/static/media/capitalone.d797f90c.png"
+  },
+  {
+    "revision": "4671bc87a2730b91ed905c59ad74c9d3",
+    "url": "/static/media/carpool.4671bc87.jpg"
+  },
+  {
+    "revision": "3fbee2773ffcd399ef48fc0d9ad4bc63",
+    "url": "/static/media/headshot.3fbee277.jpg"
+  },
+  {
+    "revision": "7f2a4666907d39c71ac97407601e25f0",
+    "url": "/static/media/lilie.7f2a4666.png"
+  },
+  {
+    "revision": "0b4443aa3debe4c1652770f96f3d5228",
+    "url": "/static/media/shell.0b4443aa.png"
+  },
+  {
+    "revision": "ba345becc6e4679af300842222f02381",
+    "url": "/static/media/twitter.ba345bec.jpg"
+  }
+]);
